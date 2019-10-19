931a3b1e05c663523465a3e059b332f05274b95b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-10-19 18:35:49 -0400

    Added the user model.

--- WebAPIPractice/Models/User.cs
+++ WebAPIPractice/Models/User.cs
@@ -1,7 +1,15 @@
-﻿namespace WebAPIPractice.Models
+﻿using System;
+
+namespace WebAPIPractice.Models
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
